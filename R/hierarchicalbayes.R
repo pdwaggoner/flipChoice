c9fc02f5393f6ca23afa25952bb41fa4350c402f
Justin Yap <justinyapcc@gmail.com>
2018-06-07 15:51:52 +1000

    Test to break flipMaxDiff

--- R/hierarchicalbayes.R
+++ R/hierarchicalbayes.R
@@ -90,6 +90,10 @@ RunStanSampling <- function(stan.dat, n.iterations, n.chains,
                             max.tree.depth, adapt.delta,
                             seed, stan.model, keep.beta, ...)
 {
+    if (!is.null(stan.dat$RSC))
+    {
+        stop("MaxDiff!")
+    }
     pars <- stanParameters(stan.dat, keep.beta)
     init <- initialParameterValues(stan.dat)
 
