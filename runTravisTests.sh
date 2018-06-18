581c0932d4532bbf7ae08263a30d605515c778c6
Mathew W. McLean <mathew.w.mclean@gmail.com>
2018-06-18 16:56:16 +1000

    Fix last commit

--- runTravisTests.sh
+++ runTravisTests.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-rcode="tfile <- 'testres.out'; capture.output(res<-devtools::test(), file = tfile); out <- readLines(tfile); cat(out, sep = '\n'); "
-rcode+="n.fail <- as.numeric(sub('Failed:[[:space:]]', '', out[grep('Failed:[[:space:]]', out)])); "
-rcode+="write.csv(as.data.frame(res), file='test_results.csv'); "
-rcode+="quit(status=n.fail, save='no');"
 echo "tfile <- 'testres.out'" > test.R
 echo "capture.output(res<-devtools::test(), file = tfile)" >> test.R
 echo "out <- readLines(tfile); cat(out, sep = '\n')" >> test.R
