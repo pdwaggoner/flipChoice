f66b1904264f0bc13e4c1a34f95fa895f6016280
Mathew W. McLean <mathew.w.mclean@gmail.com>
2018-06-18 16:43:52 +1000

    Fix Travis config

--- runTravisTests.sh
+++ runTravisTests.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-rcode="tfile <- '~/testres.out'; capture.output(res<-devtools::test(), file = tfile); out <- readLines(tfile); cat(out, sep = '\n'); "
+rcode="tfile <- 'testres.out'; capture.output(res<-devtools::test(), file = tfile); out <- readLines(tfile); cat(out, sep = '\n'); "
 rcode+="n.fail <- as.numeric(sub('Failed:[[:space:]]', '', out[grep('Failed:[[:space:]]', out)])); "
 rcode+="write.csv(as.data.frame(res), file='test_results.csv'); "
 rcode+="quit(status=n.fail, save='no');"
-Rscript --default-packages='datasets,utils,grDevices,graphics,stats,methods' -e "$rcode";
+echo "tfile <- 'testres.out'" > test.R
+echo "capture.output(res<-devtools::test(), file = tfile)" >> test.R
+echo "out <- readLines(tfile); cat(out, sep = '\n')" >> test.R
+echo "n.fail <- as.numeric(sub('Failed:[[:space:]]', '', out[grep('Failed:[[:space:]]', out)]))" >> test.R
+echo "write.csv(as.data.frame(res), file='test_results.csv')" >> test.R
+echo "quit(status=n.fail, save='no')" >> test.R
+Rscript --default-packages='datasets,utils,grDevices,graphics,stats,methods' test.R
