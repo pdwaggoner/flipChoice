28db658965227badb01e7dc5d1f7d87fcd3722c3
jakehoare <jake_hoare@hotmail.com>
2018-07-25 15:28:56 +1000

    DS-1408 Ensemble subtitle and footer. Add ensemble stats to comparison table.

--- tests/testthat/test-ensemble.R
+++ tests/testthat/test-ensemble.R
@@ -5,6 +5,6 @@ test_that("FitChoice Ensemble", {
     fit <- FitChoiceModel(experiment.data = eggs.data, hb.iterations = 100,
                           hb.chains = 1, tasks.left.out = 2)
 
-    ChoiceEnsemble(list(fit, fit, fit), compare.only = FALSE)
-    ChoiceEnsemble(list(fit, fit, fit), compare.only = TRUE)
+    expect_error(ChoiceEnsemble(list(fit, fit, fit), compare.only = FALSE), NA)
+    expect_error(ChoiceEnsemble(list(fit, fit, fit), compare.only = TRUE), NA)
 })
