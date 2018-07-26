6404285a4b2abd51907a560ccaaa0bdccb25b765
jakehoare <jake_hoare@hotmail.com>
2018-07-26 12:26:46 +1000

    DS-1408 Improve unit tests for Ensemble

--- tests/testthat/test-ensemble.R
+++ tests/testthat/test-ensemble.R
@@ -2,9 +2,16 @@ context("Ensemble")
 
 test_that("FitChoice Ensemble", {
 
-    fit <- FitChoiceModel(experiment.data = eggs.data, hb.iterations = 100,
-                          hb.chains = 1, tasks.left.out = 2)
+    fit1 <- FitChoiceModel(experiment.data = eggs.data, hb.iterations = 10,
+                   hb.chains = 1, tasks.left.out = 2,
+                   hb.warnings = FALSE, n.classes = 1)
+    fit2 <- FitChoiceModel(experiment.data = eggs.data, hb.iterations = 10,
+                   hb.chains = 1, tasks.left.out = 2,
+                   hb.warnings = FALSE, n.classes = 2)
+    fit3 <- FitChoiceModel(experiment.data = eggs.data, hb.iterations = 10,
+                   hb.chains = 1, tasks.left.out = 2,
+                   hb.warnings = FALSE, n.classes = 3)
 
-    expect_error(ChoiceEnsemble(list(fit, fit, fit), compare.only = FALSE), NA)
-    expect_error(ChoiceEnsemble(list(fit, fit, fit), compare.only = TRUE), NA)
+    expect_error(ChoiceEnsemble(list(fit1, fit2, fit3), compare.only = FALSE), NA)
+    expect_error(ChoiceEnsemble(list(fit1, fit2, fit3), compare.only = TRUE), NA)
 })
