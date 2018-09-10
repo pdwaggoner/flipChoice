6749576d89ee7e0547e8ee51f2b321b6040643c7
Justin Yap <justinyapcc@gmail.com>
2018-09-10 15:36:02 +1000

    Add cruise ship data from Sawtooth competition

---
+++ R/cruise.design.R
@@ -0,0 +1,14 @@
+#' Choice Model Design for the cruise ship data set from the 2016 Sawtooth competition
+#'
+#' An object of class \code{c("list")} with the following elements:
+#' \itemize{
+#' \item \code{design} The experimental design as a matrix
+#' \item \code{design.with.none} Same as design
+#' \item \code{attribute.levels} The attribute levels of the attributes in a list
+#' }
+#' @docType data
+#' @keywords datasets
+#' @name cruise.design
+#' @usage data(cruise.design)
+#' @format A list containing the design and attribute levels.
+NULL
