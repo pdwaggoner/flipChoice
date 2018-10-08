70759d98956424498b352ba442e398c62eefd11d
Justin Yap <justinyapcc@gmail.com>
2018-10-08 13:45:04 +1100

    DS-2017: express quadrature test differences as a proportion

--- tests/testthat/test-bayesiancriterion.R
+++ tests/testthat/test-bayesiancriterion.R
@@ -12,26 +12,34 @@ result <- ChoiceModelDesign(design.algorithm = "Random",
 prior <- matrix(c(-4:4, 0.5 * (1:9)), ncol = 2)
 design <- encodeDesign(result$design[,-1:-4], effects = FALSE)
 
-test_that("Quadrature integration vs monte carlo",
-    expect_equal(quadratureBayesianCriterion(design, prior, 18, 2, 10, 1),
-                 monteCarloBayesianCriterion(design, prior, 18, 2, 1000, 1),
-                 tolerance = 0.2))
+test_that("Quadrature integration vs monte carlo", {
+    quadrature.bc <- quadratureBayesianCriterion(design, prior, 18, 2, 10, 1)
+    monte.carlo.bc <- monteCarloBayesianCriterion(design, prior, 18,
+                                                  2, 1000, 1)
+    expect_true(abs((quadrature.bc - monte.carlo.bc) / monte.carlo.bc) < 0.01)
+})
 
 prior.2 <- prior
 prior.2[1:8, 2] <- 0
 
-test_that("Quadrature integration with zero variances vs monte carlo",
-          expect_equal(quadratureBayesianCriterion(design, prior.2, 18, 2, 10, 1),
-                       monteCarloBayesianCriterion(design, prior.2, 18, 2, 1000, 1),
-                       tolerance = 0.2))
+test_that("Quadrature integration with zero variances vs monte carlo", {
+    quadrature.bc <- quadratureBayesianCriterion(design, prior.2, 18, 2, 10, 1)
+    monte.carlo.bc <- monteCarloBayesianCriterion(design, prior.2, 18,
+                                                  2, 1000, 1)
+    expect_true(abs((quadrature.bc - monte.carlo.bc) / monte.carlo.bc) < 0.01)
+})
 
 prior.3 <- prior
 prior.3[1:8, 2] <- 0.000001
 
-test_that("Quadrature integration with zero variances vs near zero variances",
-          expect_equal(quadratureBayesianCriterion(design, prior.2, 18, 2, 10, 1),
-                       quadratureBayesianCriterion(design, prior.3, 18, 2, 10, 1),
-                       tolerance = 0.2))
+test_that("Quadrature integration with zero variances vs near zero variances", {
+    quadrature.bc.zero <- quadratureBayesianCriterion(design, prior.2, 18,
+                                                      2, 10, 1)
+    quadrature.bc.near.zero <- quadratureBayesianCriterion(design, prior.3,
+                                                           18, 2, 10, 1)
+    expect_true(abs((quadrature.bc.zero - quadrature.bc.near.zero) /
+                        quadrature.bc.zero) < 0.02)
+})
 
 test_that("Quadrature integration optimization speedup", {
     ptm <- proc.time()
